0cc13cac03c2503e75d0e9f7cf40c1853589c46a
Sergey Morozov <morozov.sergey@gmail.com>
2024-07-12 12:35:48 -0700

    brokerage totals and averages

--- src/pages/Brokerage.jsx
+++ src/pages/Brokerage.jsx
@@ -3,6 +3,7 @@ import BalanceChart from "../components/brokerage/BalanceChart";
 import CashFlowChart from "../components/brokerage/CashFlowChart";
 import Frame from "../components/Frame";
 import { parseBrokerageFile } from "../utils/brokerage";
+import { sum } from "../utils/common";
 
 // TODO: add arrow key handlers to zoom in/out and shift left/right
 export default function Brokerage({ txt }) {
@@ -24,6 +25,14 @@ export default function Brokerage({ txt }) {
   // TODO: BalanceChart can stack both start and ending balances
   // TODO: charts use canvas, the size of which can't be easily set with tailwind
   const render = (slice) => {
+    const columns = ["deposits", "withdrawals", "market change minus fees", "dividends & interest"];
+
+    const totals = columns.reduce((acc, name) => {
+      acc[name] = sum(slice.map((fields) => parseInt(fields[name])));
+      return acc;
+    }, {});
+    // console.log(totals);
+
     return (
       <div className="w-full flex flex-col justify-center items-center">
         <div className="w-3/5">
@@ -31,6 +40,19 @@ export default function Brokerage({ txt }) {
         </div>
         <div className="w-3/5">
           <CashFlowChart transactions={slice} title="cash flow" />
+        </div>
+
+        {columns.map((name) => (
+          <div key={name} className="text-lg">
+            {name}: {totals[name]}, {Math.round(totals[name] / slice.length)}/mo
+          </div>
+        ))}
+
+        <div className="text-lg">
+          Deposits + Withdrawals {Math.round((totals["deposits"] + totals["withdrawals"]) / 1000)}k
+        </div>
+        <div className="text-lg">
+          Market + Income {totals["market change minus fees"] + totals["dividends & interest"]}
         </div>
       </div>
     );
