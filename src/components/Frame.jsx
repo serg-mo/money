4d513ab60d95898f11babd98971864d1ed0b0090
Sergey Morozov <morozov.sergey@gmail.com>
2024-10-20 12:29:53 -0700

    page search now works

--- src/components/Frame.jsx
+++ src/components/Frame.jsx
@@ -13,15 +13,16 @@ export default function Frame({
   // NOTE: keyboard arrows effectively control both edges of the timeline
   // NOTE: stateless because it's easy to navigate back to where you just were
   const handleKeyPress = (event) => {
-    event.preventDefault();
-
+    // NOTE: I really like this kind of navigation
     // NOTE: all of these must be closures
     if (event.key === 'ArrowLeft') {
       setLeft((prev) => Math.max(prev - 1, 0));
+      event.preventDefault();
     } else if (event.key === 'ArrowRight') {
       setLeft((prev) =>
         Math.min(prev + 1, transactions.length - Math.min(size, 2))
       ); // 2..size
+      event.preventDefault();
     } else if (event.key === 'ArrowUp') {
       setSize((prev) => {
         const newSize = Math.min(prev + 1, transactions.length);
@@ -31,8 +32,10 @@ export default function Frame({
         }
         return newSize;
       });
+      event.preventDefault();
     } else if (event.key === 'ArrowDown') {
       setSize((prev) => Math.max(prev - 1, minSize));
+      event.preventDefault();
     }
   };
 
